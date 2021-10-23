3d4617a3243068ae2fad30778cafac4b2301ccb1
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2021-10-23 15:05:00 +0700

    Move start to bottom

--- AppCoordinatorExample/AppDirector.swift
+++ AppCoordinatorExample/AppDirector.swift
@@ -28,22 +28,22 @@ final class AppDirector {
     // We don't need to retain/release the coordinator (LoginNavigationController) here
     // because the navigation stack already manages this for us. 😎
     let coordinator = LoginNavigationController(presenterViewController: navigationController)
-    coordinator.start()
     coordinator.completion = { coordinator in
       print("Login result: \(coordinator.result)")
     }
+    coordinator.start()
   }
 
   func showPurchaseFlow() {
     // We have to manually retain PurchaseCoordinator because is not a view controller subclass.
     // So the navigation stack cannot help retain it for us. 😢
     let coordinator = PurchaseCoordinator(presenterViewController: navigationController)
-    coordinator.start()
     coordinator.completion = { [weak self] coordinator in
       print("Purchase result: \(coordinator.result)")
       self?.purchaseCoordinator = nil // release the coordinator
     }
     self.purchaseCoordinator = coordinator // retain the coordinator
+    coordinator.start()
   }
 
 }
