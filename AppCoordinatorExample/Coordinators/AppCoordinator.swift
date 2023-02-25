8329eb22599638fa735535e1a8a4899909ad79c6
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2023-02-26 01:02:28 +0800

    Clean up code

--- AppCoordinatorExample/Coordinators/AppCoordinator.swift
+++ AppCoordinatorExample/Coordinators/AppCoordinator.swift
@@ -2,17 +2,15 @@ import UIKit
 
 @MainActor final class AppCoordinator {
 
-  private var window: UIWindow!
+  private let window: UIWindow
 
   init(window: UIWindow) {
     self.window = window
 
     // Note:
-    // Need to set a dummy view controller to window.rootViewController here because
-    // in coordinator.start(), where the correct viewController is set up, is an async task.
-    // App will crash if rootViewController at the end of application didFinishLaunchingWithOptions.
-    // It will be briefly visible, which should be ok for demo purpose.
-    // In a real production app, you may want to handle this more gracefully.
+    // Need to set a dummy view controller to window.rootViewController here because the rootViewController
+    // set up in coordinator.start() is async. It won't finish when application didFinishLaunchingWithOptions ends
+    // and app will crash.
     let dummyViewController = UIViewController()
     window.rootViewController = dummyViewController
     window.makeKeyAndVisible()
@@ -33,6 +31,8 @@ import UIKit
     Task {
       let coordinator = HomeCoordinator(window: window)
       try? await coordinator.start()
+
+      // When HomeCoordinator ends, it means we have logged out.
       UserDefaults.standard.isLoggedIn = false
       start()
     }
@@ -42,6 +42,8 @@ import UIKit
     Task {
       let coordinator = LoginCoordinator(window: window)
       let result = try? await coordinator.start()
+
+      // When LoginCoordinator ends, it means we have successfully logged in.
       print("Login result: \(String(describing: result))")
       UserDefaults.standard.isLoggedIn = true
       start()
