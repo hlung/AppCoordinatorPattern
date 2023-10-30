79d6b8d8f9bba5ad936b56e521598c9dbd30e69e
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2023-10-30 18:18:12 +0800

    Update a comment

--- AppCoordinatorExample/Coordinators/Coordinator.swift
+++ AppCoordinatorExample/Coordinators/Coordinator.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 protocol Coordinator: AnyObject {
-  var window: UIWindow { get } // This can be rootViewController. But for this demo purpose, it's easier to just use window.
+  // This can be just a rootViewController. But for this demo purpose, it's easier to just use window.
+  // A window also has a rootViewController, so it kinda works for now.
+  var window: UIWindow { get }
 
   func start()
 }
