68a813d517d6d172b96a017788152236db0f6850
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2023-10-31 16:12:46 +0800

    Update comments

--- AppCoordinatorExample/Coordinators/Coordinator.swift
+++ AppCoordinatorExample/Coordinators/Coordinator.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+/**
+ What is a coordinator?
+ "A coordinator is an object that bosses one or more view controllers around.
+ Taking all of the driving logic out of your view controllers."
+ - from https://khanlou.com/2015/10/coordinators-redux/
+ */
 protocol Coordinator: AnyObject {
   func start()
 }
