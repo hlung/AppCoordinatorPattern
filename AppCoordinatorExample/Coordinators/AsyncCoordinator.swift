a775fc60c945c7dd27cd817c9bca0d44eada3787
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2023-11-10 12:59:28 +0800

    Add comment

--- AppCoordinatorExample/Coordinators/AsyncCoordinator.swift
+++ AppCoordinatorExample/Coordinators/AsyncCoordinator.swift
@@ -14,6 +14,10 @@ protocol ParentAsyncCoordinator: AsyncCoordinator {
 
 extension ParentAsyncCoordinator {
   func start<C: AsyncCoordinator>(_ coordinator: C) async throws -> C.Output {
+    // Here, appending child coordinator is not for lifecycle management reasons. Swift concurrency already handles it.
+    // It's to provide the information of what coordinator is running, to handle use cases like ignoring some deeplinks.
+    // It's not a use case Viki has right now. We always reset everything to home page and always handle that deeplink.
+    // But just in case we need to do it in future.
     childAsyncCoordinators.append(coordinator)
     let output = try await coordinator.start()
     childAsyncCoordinators.removeAll(where: { $0 === coordinator })
