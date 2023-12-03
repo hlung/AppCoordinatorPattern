59d45e7a7811010e83828728792162fd015f42cb
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2023-12-03 22:05:56 +0800

    Minor

--- AppCoordinatorExample/Core/Controllers/APIClient.swift
+++ AppCoordinatorExample/Core/Controllers/APIClient.swift
@@ -3,7 +3,17 @@ import Foundation
 class APIClient: AppLaunchDataProvider {
 
   func getAppLaunchData() async throws -> Data {
-    try await Task.sleep(for: .seconds(1))
+    // Perform various async calls that happens at app launch/login (stuff in Viki Prefetcher):
+    // - fetch config cat
+    // - fetch latest session info from viki backend
+    // - call DeviceController.registerDeviceAndCapabilitiesIfNeeded()
+    // - call refreshSubscribedResourceIDs
+    // - set up offlineViewingController
+    // - set up watchlistController
+    // - set up watchMarkersController
+    // - set up rentalPlaybackMarkersController
+
+    try await Task.sleep(for: .seconds(2))
     return Data()
   }
 
